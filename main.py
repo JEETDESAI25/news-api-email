bb750b823d00d7f21ebbaf4de75062f65c858a0f
JEETDESAI25 <jeetdesai25@gmail.com>
2023-01-04 21:16:31 -0800

    send an email with news titles and description.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "3f5ea31f064b425fbee90949cd7a3ef4"
 
@@ -11,7 +12,12 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article title and description
+body = ""
+
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
-    
+
+    body = body + article["title"] + "\n" + article["description"] + 2 * "\n"
+
+
+body = body.encode("utf-8")
+send_email(message=body)
